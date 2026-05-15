3b8f0107c94fd50a3667341668712c6b4ed7d536
ArtsiomYuskovets <150152122+ArtsiomYuskovets@users.noreply.github.com>
2026-05-15 18:02:36 +0300

    feat(routes): add protected route for creating new project

---
+++ app/(protected)/projects/new/page.tsx
@@ -0,0 +1,9 @@
+import { ProjectCreateForm } from "@/components/projectCreate/ProjectCreateForm";
+
+export default function NewProjectPage() {
+  return (
+    <main>
+      <ProjectCreateForm />
+    </main>
+  );
+}
